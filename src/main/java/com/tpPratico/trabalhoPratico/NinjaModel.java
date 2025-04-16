476d0efc38f0e0ce7c99aee90870dddc79d43085
ToninOgarotoDePrograma <tonycaldeira2007@gmail.com>
2025-04-15 23:17:15 -0300

    Cria a entidade no ninjamodel bd

---
+++ src/main/java/com/tpPratico/trabalhoPratico/NinjaModel.java
@@ -0,0 +1,54 @@
+package com.tpPratico.trabalhoPratico;
+
+import jakarta.persistence.*;
+
+
+//classe em entidade do BD
+//jpa = java persistence api
+
+@Entity // bd
+@Table(name = "tb_cadastro")
+public class NinjaModel {
+
+    @Id //defini o id, primeira variavel abaixo
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto incremental
+    private Long id;// <- esta
+    private String nome;
+    private String email;
+    private int idade;
+
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+}
